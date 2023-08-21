6609ff47939d4597d72159f98506ae3e097d5b8a
Fabrizio Gasparini <makerfaffa@gmail.com>
2023-08-21 21:47:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", 'r') as f:
+    long_desc = f.read()
+
 setup(
     name="ClasseVivaAPI",
     version="0.1",
-    author="Fabrizio Gasparini",
+    author="MakerFaffa",
     author_email="gasparini.fabrizio@einaudicorreggio.it",
-    description="Questa libreria Python permette di utlizzare l'API di ClasseViva - GruppoSpaggiariParma",
-    long_description="Questa libreria Python permette di utlizzare l'API di ClasseViva - GruppoSpaggiariParma",
+    description="ClasseVivaAPI è una libreria Python che permette di utlizzare l'API di Classeviva - GruppoSpaggiariParma per ottenere informazioni come Voti, Note e Documenti, relative ad un account ClasseViva.",
+    long_description= long_desc,
     long_description_content_type="text/markdown",
     url="https://github.com/MakerFaffa/ClasseVivaAPI",
     packages=find_packages(),
@@ -16,5 +19,10 @@ setup(
         "ClasseVivaAPI"
     ],
     install_requires=["requests"],
-    python_requires=">=3.10"
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Licence :: OSI Approved :: Mit Licence",
+        "Operating System :: OS Indipendent"
+    ]
 )
